27cf81a922fd176c914d2cc86a0988095e4b0f97
SarimMalik01 <sarimmalik21.06@gmail.com>
2024-11-09 00:07:45 +0530

    Added developername

--- vanlife/src/Footer.js
+++ vanlife/src/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./css/About.css";
-import { FaCopyright } from 'react-icons/fa'; 
+import { FaCopyright, FaGithub } from 'react-icons/fa';
 
 function Footer() {
     return (
@@ -11,6 +11,19 @@ function Footer() {
                 </span>
                 2024 #VANLIFE Copyrights reserved
             </h3>
+            
+            {/* Add hover effect to this text */}
+            <h2 className="developer-name">Developed by Sarim Malik</h2>
+            
+            {/* GitHub link with hover effect */}
+            <a 
+                href="https://github.com/SarimMalik01/WebDevelopment-Projects/tree/main/vanlife"
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="github-link"
+            >
+                <FaGithub className="github-icon" /> Visit Project
+            </a>
         </div>
     );
 }
